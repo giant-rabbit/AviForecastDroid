a705ac6c2bda29f32cb960feb86228bf3d631bc8
Seth Pollack <seth@pollackphoto.com>
2012-10-22 15:05:07 -0700

    cleanup

--- src/com/sebnarware/avalanche/AviLevel.java
+++ src/com/sebnarware/avalanche/AviLevel.java
@@ -1,11 +1,10 @@
 package com.sebnarware.avalanche;
 
 public class AviLevel {
-
-	public final int AVI_LEVEL_UNKNOWN = 0;
-	public final int AVI_LEVEL_LOW = 1;
-	public final int AVI_LEVEL_MODERATE = 2;
-	public final int AVI_LEVEL_CONSIDERABLE = 3;
-	public final int AVI_LEVEL_HIGH = 4;
-	public final int AVI_LEVEL_EXTREME = 5;
+	public final int UNKNOWN = 0;
+	public final int LOW = 1;
+	public final int MODERATE = 2;
+	public final int CONSIDERABLE = 3;
+	public final int HIGH = 4;
+	public final int EXTREME = 5;
 }
