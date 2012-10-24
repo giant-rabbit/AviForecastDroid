8f2a3902ba7e56f59dc638924fc93ab5482eadb9
Seth Pollack <seth@pollackphoto.com>
2012-10-24 16:16:53 -0700

    add app icon

--- gen/com/sebnarware/avalanche/R.java
+++ gen/com/sebnarware/avalanche/R.java
@@ -11,8 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int ic_maps_indicator_current_position=0x7f020001;
+        public static final int ic_maps_indicator_current_position=0x7f020000;
+        public static final int logo=0x7f020001;
     }
     public static final class id {
         public static final int mapview=0x7f050001;
@@ -23,13 +23,10 @@ public final class R {
         public static final int main=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int button_today=0x7f040002;
-        public static final int button_tomorrow=0x7f040003;
-        public static final int button_two_days_out=0x7f040004;
-        public static final int hello=0x7f040000;
-        public static final int hello_world=0x7f040005;
-        public static final int menu_settings=0x7f040006;
-        public static final int title_activity_web_view=0x7f040007;
+        public static final int app_name=0x7f040000;
+        public static final int button_today=0x7f040001;
+        public static final int button_tomorrow=0x7f040002;
+        public static final int button_two_days_out=0x7f040003;
+        public static final int title_activity_web_view=0x7f040004;
     }
 }
