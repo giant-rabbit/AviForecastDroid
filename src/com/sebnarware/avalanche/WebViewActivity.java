3a55a48ed57efc9986ca5c956566f77c01451768
Seth Pollack <seth@pollackphoto.com>
2012-10-26 15:12:36 -0700

    make webview work better

--- src/com/sebnarware/avalanche/WebViewActivity.java
+++ src/com/sebnarware/avalanche/WebViewActivity.java
@@ -2,9 +2,13 @@ package com.sebnarware.avalanche;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings.ZoomDensity;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 
+@SuppressLint("SetJavaScriptEnabled")
 public class WebViewActivity extends Activity {
 
     @Override
@@ -17,9 +21,19 @@ public class WebViewActivity extends Activity {
         Intent intent = getIntent();
         String url = intent.getStringExtra(MainActivity.INTENT_EXTRA_WEB_VIEW_URL);
         
-        // get the web view, and load the url
         WebView webView = (WebView) findViewById(R.id.webview);
+        
+        // set a reasonable zoom and view mode
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setDefaultZoom(ZoomDensity.FAR);
         webView.getSettings().setBuiltInZoomControls(true);
+        
+        // make link navigation stay within this webview, vs. launching the browser
+        webView.setWebViewClient(new WebViewClient());
+        
+        // enable javascript
+        webView.getSettings().setJavaScriptEnabled(true);
+        
         webView.loadUrl(url);
     }
 
