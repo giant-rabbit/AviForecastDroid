e0808d82608864040f86a635645112e87512ad67
Seth Pollack <seth@pollackphoto.com>
2012-11-16 12:05:03 -0800

    always use our own draw method, as it reduces the redraw frequency

--- src/com/sebnarware/avalanche/FixedMyLocationOverlay.java
+++ src/com/sebnarware/avalanche/FixedMyLocationOverlay.java
@@ -14,17 +14,11 @@ import com.google.android.maps.MyLocationOverlay;
 import com.google.android.maps.Projection;
 
 /**
- * Fixes bug with some phone's location overlay class (ie Droid X).
- * Essentially, it attempts to use the default MyLocationOverlay class,
- * but if it fails, we override the drawMyLocation method to provide
- * an icon and accuracy circle to mimic showing user's location.  Right
- * now the icon is a static image.  If you want to have it animate, modify
- * the drawMyLocation method.
+ * works around bugs with some phone's location overlay class (ie Droid X throwing on draw; 
+ * or other phones redrawing nearly continuously)
  */
 public class FixedMyLocationOverlay extends MyLocationOverlay {
-	
-	private boolean bugged = false;
-	
+		
 	private Drawable drawable;
 	private Paint accuracyPaint;
 	private Point center;
@@ -37,56 +31,45 @@ public class FixedMyLocationOverlay extends MyLocationOverlay {
 	}
 	
 	@Override
-	protected void drawMyLocation(Canvas canvas, MapView mapView,
-			Location lastFix, GeoPoint myLocation, long when) {
-		if(!bugged) {
-			try {
-				super.drawMyLocation(canvas, mapView, lastFix, myLocation, when);
-			} catch (Exception e) {
-				// we found a buggy phone, draw the location icons ourselves
-				bugged = true;
-			}
+	protected void drawMyLocation(Canvas canvas, MapView mapView, Location lastFix, GeoPoint myLocation, long when) {
+	
+		if (drawable == null) {
+			
+			accuracyPaint = new Paint();
+			accuracyPaint.setAntiAlias(true);
+			accuracyPaint.setStrokeWidth(2.0f);
+			
+			drawable = mapView.getContext().getResources().getDrawable(R.drawable.ic_maps_indicator_current_position);
+			width = drawable.getIntrinsicWidth();
+			height = drawable.getIntrinsicHeight();
+			center = new Point();
+			left = new Point();
 		}
 		
-		if(bugged) {
-			if(drawable == null) {
-				
-				accuracyPaint = new Paint();
-				accuracyPaint.setAntiAlias(true);
-				accuracyPaint.setStrokeWidth(2.0f);
-				
-				drawable = mapView.getContext().getResources().getDrawable(R.drawable.ic_maps_indicator_current_position);
-				width = drawable.getIntrinsicWidth();
-				height = drawable.getIntrinsicHeight();
-				center = new Point();
-				left = new Point();
-			}
-			
-			Projection projection = mapView.getProjection();
-			double latitude = lastFix.getLatitude();
-			double longitude = lastFix.getLongitude();
-			float accuracy = lastFix.getAccuracy();
-			
-			float[] result = new float[1];
-			
-			Location.distanceBetween(latitude, longitude, latitude, longitude + 1, result);
-			float longitudeLineDistance = result[0];
-			
-			GeoPoint leftGeo = new GeoPoint((int)(latitude*1e6), (int)((longitude-accuracy/longitudeLineDistance)*1e6));
-			projection.toPixels(leftGeo, left);
-			projection.toPixels(myLocation, center);
-			int radius = center.x - left.x;
-			
-			accuracyPaint.setColor(0xff6666ff);
-			accuracyPaint.setStyle(Style.STROKE);
-			canvas.drawCircle(center.x, center.y, radius, accuracyPaint);
-			
-			accuracyPaint.setColor(0x186666ff);
-			accuracyPaint.setStyle(Style.FILL);
-			canvas.drawCircle(center.x, center.y, radius, accuracyPaint);
-			
-			drawable.setBounds(center.x - width/2, center.y - height/2, center.x + width/2, center.y + height/2);
-			drawable.draw(canvas);
-		}
+		Projection projection = mapView.getProjection();
+		double latitude = lastFix.getLatitude();
+		double longitude = lastFix.getLongitude();
+		float accuracy = lastFix.getAccuracy();
+		
+		float[] result = new float[1];
+		
+		Location.distanceBetween(latitude, longitude, latitude, longitude + 1, result);
+		float longitudeLineDistance = result[0];
+		
+		GeoPoint leftGeo = new GeoPoint((int)(latitude*1e6), (int)((longitude-accuracy/longitudeLineDistance)*1e6));
+		projection.toPixels(leftGeo, left);
+		projection.toPixels(myLocation, center);
+		int radius = center.x - left.x;
+		
+		accuracyPaint.setColor(0xff6666ff);
+		accuracyPaint.setStyle(Style.STROKE);
+		canvas.drawCircle(center.x, center.y, radius, accuracyPaint);
+		
+		accuracyPaint.setColor(0x186666ff);
+		accuracyPaint.setStyle(Style.FILL);
+		canvas.drawCircle(center.x, center.y, radius, accuracyPaint);
+		
+		drawable.setBounds(center.x - width/2, center.y - height/2, center.x + width/2, center.y + height/2);
+		drawable.draw(canvas);
 	}
 }
